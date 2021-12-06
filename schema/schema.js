1799ccc3d8d59c65b967e0d1aa8387a6f8de8172
Madhawa Jayagoda <nipunajayagoda@gmail.com>
2021-12-06 21:58:41 +0530

    schema added v1

---
+++ schema/schema.js
@@ -0,0 +1,36 @@
+const graphql = require('graphql')
+const _ = require('lodash')
+
+const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+
+var books = [
+    { name: 'Name of the Wind', genre: 'Fantasy', id: '1'},
+    { name: 'The Final Empire', genre: 'Fantasy', id: '2' },
+    { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' }
+];
+
+const BookType = new GraphQLObjectType({
+    name: 'Book',
+    fields: () => ({
+        id: { type: GraphQLString },
+        name: { type: GraphQLString},
+        genre: { type: GraphQLString} 
+    })
+});
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: { id: { type: GraphQLString}},
+            resolve(parents, args) {
+                _.find(books)
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
