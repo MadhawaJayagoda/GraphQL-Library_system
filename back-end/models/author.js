f83b6008296a8241d3d94e3f4975d1c0d20673a4
Madhawa Jayagoda <nipunajayagoda@gmail.com>
2021-12-30 21:13:03 +0530

    database schemas added

---
+++ back-end/models/author.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+module.exports = mongoose.model('authors', authorSchema);
